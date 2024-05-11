9438af87a385b91d603abe8df16641f1cb338a6d
wrd1stProgrammer <kicoa24@gmail.com>
2024-05-11 19:01:34 +0900

    user

--- backend/diaryapp/models.py
+++ backend/diaryapp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User=get_user_model()
 class DiaryApp(models.Model):
-    #id = models.AutoField(primary_key=True)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=45)
     content = models.TextField(max_length=3000)
