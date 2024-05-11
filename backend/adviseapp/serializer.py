074965e90018de40f0c980b532cde9eb9202d1cb
triant11 <jin6562@gmail.com>
2024-05-11 21:43:51 +0900

    traint11

---
+++ backend/adviseapp/serializer.py
@@ -0,0 +1,8 @@
+# AdviceApp/serializers.py
+from rest_framework import serializers
+from .models import Advice
+
+class AdviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Advice
+        fields = ['id', 'target_diary', 'gpt_advice', 'gpt_recommendation']
