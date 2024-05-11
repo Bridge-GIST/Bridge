31bedebcb68cfb3347ad34c8851f893a2acc1ef4
triant11 <jin6562@gmail.com>
2024-05-12 06:41:12 +0900

    navigationBar

--- frontend/vite-project/src/navigationBar/navigationBar.jsx
+++ frontend/vite-project/src/navigationBar/navigationBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./navigationBar.css";
 
 function NavigationBar() {
   return (
@@ -9,6 +10,7 @@ function NavigationBar() {
           <span className="B">B</span>
           <span className="ridge">ridge</span>
         </Link>
+
         <Link to="/mainScreen" className="diary">
           <span className="DiaryBtn">Diary</span>
         </Link>
